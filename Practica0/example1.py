c9ee807175341095292cfed851a194429f43f5a4
alejandrolalaguna <alejandrolalaguna@gmail.com>
2024-01-25 12:05:49 +0100

    Modificar tamaño ventana

--- Practica0/example1.py
+++ Practica0/example1.py
@@ -1,11 +1,27 @@
-# Example 1: read and show an image
-import cv2 # Import python-supported OpenCV functions
-import numpy as np # Import numpy and call it np
-from matplotlib import pyplot as plt # Import pyplot and call it plt
-img = cv2.imread('cilindros.png', cv2.IMREAD_GRAYSCALE)
-cv2.namedWindow( 'Example1', cv2.WINDOW_AUTOSIZE )
-cv2.imshow('Example1',img)
-print(type(img))
-print(img.shape)
-cv2.waitKey(0)
-cv2.destroyWindow( 'Example1' ) # cv2.destroyAllWindows()
+import cv2
+import numpy as np
+
+# crear el main
+if __name__ == "__main__":
+    # Read the image
+    img = cv2.imread('Mari.jpeg', cv2.IMREAD_GRAYSCALE)
+
+    # Create a named window
+    cv2.namedWindow('Example1', cv2.WINDOW_NORMAL)  # WINDOW_NORMAL allows resizing
+
+    # Resize the window to a smaller size
+    cv2.resizeWindow('Example1', 1024, 1024)  # Adjust size as needed
+
+    # Display the image
+    cv2.imshow('Example1', img)
+
+    # Wait for a key press
+    cv2.waitKey(0)
+
+    # Destroy the window
+    cv2.destroyWindow('Example1')
+
+    # Finalizar el programa
+    exit(0)
+
+    
--- Practica0/example2.py
+++ Practica0/example2.py
@@ -4,18 +4,20 @@ import numpy as np # Import numpy and call it np
 from matplotlib import pyplot as plt # Import pyplot and call it plt
 
 def show_image_properties(my_img):
-    cv2.namedWindow( 'Example1', cv2.WINDOW_AUTOSIZE )
+    cv2.namedWindow( 'Example1', cv2.WINDOW_NORMAL)
+    cv2.resizeWindow('Example1', 1024, 1024)  # Adjust size as needed
     cv2.imshow('Example1',my_img)
     print("Properties of the matrix used to store the image")
     print("They are numpy arrays: type(my_img)= ", type(my_img))
     print("Rows, columns and channels: my_img.shape= ", my_img.shape)
     print("Total number of pixels: my_img.size= ", my_img.size)
     print("Image datatype: my_img.dtype = ", my_img.dtype )
-    cv2.waitKey(1000) #cv2.waitKey(0)
+    cv2.waitKey(5000) #cv2.waitKey(0)
+
     cv2.destroyWindow( 'Example1' ) # cv2.destroyAllWindows()
     return(0)
 
-img = cv2.imread('cilindros.png', cv2.IMREAD_GRAYSCALE)
-img_bgr = cv2.imread('cilindros.png', cv2.IMREAD_COLOR)
+img = cv2.imread('Mari.jpeg', cv2.IMREAD_GRAYSCALE)
+img_bgr = cv2.imread('Mari.jpeg', cv2.IMREAD_COLOR)
 show_image_properties(img)
 show_image_properties(img_bgr)
